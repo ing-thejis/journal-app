f1625ac475061becfe0c517a00b17b9c8655726b
ing-thejis <ing.jesithgarcia@gmail.com>
2022-01-31 08:08:47 -0500

    login journal-app

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { BrowserRouter as Router, Navigate, Routes, Route } from 'react-router-dom'
 import AuthRouter from './AuthRouter';
 import JournalScreen from '../components/journal/JournalScreen';
+import { firebase } from '../firebase/firebaseConfig'
+import { useDispatch } from 'react-redux'
+import { login } from '../actions/auth'
 
 const AppRouter = () => {
+
+	const dispatch = useDispatch()
+
+	const [checking, setChecking] = useState(true)
+	const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+	useEffect(() => {
+		
+		firebase.auth().onAuthStateChanged( (user) => {
+
+			if( user?.uid ) {
+				dispatch( login(user.uid, user.displayName) )
+				setIsLoggedIn( true )
+			} else {
+				setIsLoggedIn(false)
+			}
+
+			setChecking(false)
+
+		})
+	
+	}, [ setChecking, setIsLoggedIn ])
+
+	if ( checking ) {
+		return (
+			<h1>loading...</h1>
+		)
+	}
+
 	return (
 		<Router>
 		<div>
